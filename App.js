14740c8a5aa3eca221ec681213cbee7f0c474660
Wilyarti Howard <wilyarti@gmail.com>
2019-12-28 17:43:50 +1000

    Minor fixes.

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {Text, View, ListItem, Image} from 'react-native';
+import {Text, View, ListItem, Image, Button} from 'react-native';
 import {BleManager} from 'react-native-ble-plx';
 import {ToastAndroid} from 'react-native';
 import {Buffer} from 'buffer';
 import {headerImage} from './favicon.png';
+import moment from 'moment';
 const airMonitorServiceUUID = 'db101875-d9c4-4c10-b856-fad3a581a6ea';
 const tempCharacteristicUUID = '06576524-99f9-4dc5-b6ea-c66dc433e6f2';
 const co2CharacteristicUUID = '4e1fb0da-dc91-43ea-9b6d-77f699ddbbed';
@@ -23,10 +24,12 @@ export default class HelloWorldApp extends Component {
     super();
     this.manager = new BleManager();
     this.state = {
+      deviceID: '00:00:00:00:00:00',
       devices: [],
       connected: false,
       co2: 0,
       temp: 0,
+      lastUpdate: '',
     };
   }
   async setupNotifications(device) {
@@ -37,13 +40,17 @@ export default class HelloWorldApp extends Component {
         if (error) {
           console.log(error.message);
           ToastAndroid.show(error.message, ToastAndroid.SHORT);
+          this.setState({connected: false});
+
           return;
         }
         let converted = Buffer.from(characteristic.value, 'base64').toString(
           'ascii',
         );
         console.log('Got value: ' + converted);
-        this.setState({temp: converted});
+        let lastUpdate = moment(new Date());
+        this.setState({temp: converted, lastUpdate});
+        this.setState({connected: true});
       },
     );
     device.monitorCharacteristicForService(
@@ -53,17 +60,28 @@ export default class HelloWorldApp extends Component {
         if (error) {
           console.log(error.message);
           ToastAndroid.show(error.message, ToastAndroid.SHORT);
+          this.setState({connected: false});
+
           return;
         }
         let converted = Buffer.from(characteristic.value, 'base64').toString(
           'ascii',
         );
         console.log('Got value: ' + converted);
-        this.setState({co2: converted});
+        let lastUpdate = moment(new Date());
+        this.setState({co2: converted, lastUpdate});
+        this.setState({connected: true});
       },
     );
   }
-
+  handleConnectButton() {
+    console.log('Handling button press');
+    if (this.state.connected) {
+      this.manager.cancelDeviceConnection(this.state.deviceID);
+    } else {
+      this.scanAndConnect();
+    }
+  }
   componentDidMount() {
     const subscription = this.manager.onStateChange(state => {
       if (state === 'PoweredOn') {
@@ -79,6 +97,7 @@ export default class HelloWorldApp extends Component {
 
       if (error) {
         this.error(error.message);
+        this.setState({connected: false});
         return;
       }
 
@@ -93,12 +112,12 @@ export default class HelloWorldApp extends Component {
           })
           .then(device => {
             console.log('Setting notifications');
+            this.setState({deviceID: device.id});
             return this.setupNotifications(device);
           })
           .then(
             () => {
               console.log('Listening...');
-              this.setState({connected: true});
             },
             error => {
               console.log(error.message);
@@ -111,6 +130,9 @@ export default class HelloWorldApp extends Component {
 
   render() {
     const list = this.state.devices;
+    const lastUpdate = this.state.lastUpdate
+      ? moment(this.state.lastUpdate).fromNow()
+      : 'Not connected';
     return (
       <View
         style={{
@@ -137,6 +159,11 @@ export default class HelloWorldApp extends Component {
           </Text>
           <Text>Temperature: {this.state.temp}</Text>
           <Text>CO2: {this.state.co2}</Text>
+          <Text>Last update: {lastUpdate}</Text>
+          <Button
+            title={this.state.connected ? 'Disconnect' : 'Connect'}
+            onPress={() => this.handleConnectButton()}
+          />
         </View>
         <View style={{}}>
           <Text>{this.state.connected ? 'Connected' : 'Not connected'}</Text>
